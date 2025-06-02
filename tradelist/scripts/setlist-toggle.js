727d31f7b305845faeb47f083105d8a1670ff070
MegaVortex <MegaVortex@users.noreply.github.com>
2025-06-02 19:54:23 +0000

    deploy: e43aa5cfd13667d95c26365d5fc6f3ac81d9af45

--- tradelist/scripts/setlist-toggle.js
+++
@@ -1,6 +0,0 @@
-function toggleFeatInfo(el) {
-    const popup = el.nextElementSibling;
-    if (popup) {
-        popup.style.display = popup.style.display === 'inline' ? 'none' : 'inline';
-    }
-}
